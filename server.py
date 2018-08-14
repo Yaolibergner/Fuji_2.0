727e9b708614fd2616dcc0c3bf508fe9e2ed264f
Yao Li <melissayyl@gmail.com>
2018-08-14 01:02:39 +0000

    Update fujiapp to write draft version 1st component.

--- server.py
+++ server.py
@@ -3,24 +3,25 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 from flask_socketio import send, emit
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
-@app.route('/')
-def index():
-    return render_template('test.html')
+# @app.route('/')
+# def index():
+#     return render_template('test.html')
 
 # Serverside event handler on an unnamed event
 # Namespace is to allow multiplex connections
 # Broadcast=True allows multiple clients. Which can estiblish chat between
 # each other. 
 
-@socketio.on('my event', namespace='/test')
-def test_message(message):
-    emit('my receive', {'data': message['data']}, broadcast=True)
+@socketio.on('update', namespace='/test')
+def send_message(message):
+    emit('response', {'data': message['data']}, broadcast=True)
 
 
 if __name__ == '__main__':
